1303fe4ec00f9cc2b41ad9e66c0f20ec2023e808
Sergio López Millán <lopezmillansergio@gmail.com>
2020-11-07 23:06:32 +0100

    Init commit

---
+++ src/main/kotlin/com/slopezmill/kotlinbasicapi/KotlinbasicapiApplication.kt
@@ -0,0 +1,11 @@
+package com.slopezmill.kotlinbasicapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinbasicapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinbasicapiApplication>(*args)
+}
---
+++ src/test/kotlin/com/slopezmill/kotlinbasicapi/KotlinbasicapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.slopezmill.kotlinbasicapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinbasicapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
