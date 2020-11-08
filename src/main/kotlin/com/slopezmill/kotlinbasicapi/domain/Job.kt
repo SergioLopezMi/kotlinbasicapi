1ecce310f3193bc55fc99a4c2dc33f838640462a
Sergio López Millán <lopezmillansergio@gmail.com>
2020-11-08 05:59:29 +0100

    Simplifiación de las clases

--- src/main/kotlin/com/slopezmill/kotlinbasicapi/domain/Job.kt
+++ src/main/kotlin/com/slopezmill/kotlinbasicapi/domain/Job.kt
@@ -2,5 +2,4 @@ package com.slopezmill.kotlinbasicapi.domain
 
 import java.time.LocalDateTime
 
-class Job (var id: Long, var name: String, var initDate: LocalDateTime, var isEnded: Boolean, var endDate: LocalDateTime?) {
-}
+class Job(var id: Long? = null, var name: String = "", var initDate: LocalDateTime? = null, var isEnded: Boolean = false, var endDate: LocalDateTime? = null)
