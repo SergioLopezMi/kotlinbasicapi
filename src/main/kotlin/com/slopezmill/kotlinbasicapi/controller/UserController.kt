3250fd46cb9c367a8049b527b9abbbe28d600601
Sergio López Millán <lopezmillansergio@gmail.com>
2020-11-09 00:51:37 +0100

    Añadido UserController

--- src/main/kotlin/com/slopezmill/kotlinbasicapi/controller/UserController.kt
+++ src/main/kotlin/com/slopezmill/kotlinbasicapi/controller/UserController.kt
@@ -6,6 +6,7 @@ import com.slopezmill.kotlinbasicapi.dto.UserDto
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseBody
 
 
 interface UserController {
@@ -13,4 +14,8 @@ interface UserController {
     fun findUserById(@PathVariable id: Int): ResponseEntity<User>
     fun addUser(@RequestBody userDto: UserDto): ResponseEntity<User>
     fun addJob(@PathVariable id: Int, @RequestBody jobDto: JobDto): ResponseEntity<User>
+    fun updateUser(@PathVariable id:Int, @RequestBody userDto: UserDto): ResponseEntity<User>
+    fun updateUserJob(@PathVariable userId:Int, @RequestBody jobDto: JobDto): ResponseEntity<User>
+    fun deleteUser(@PathVariable id:Int): Boolean
+    fun deleteUserJob(@PathVariable userId: Int, @RequestBody jobDto: JobDto): Boolean
 }
